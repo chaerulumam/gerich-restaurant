0839232708f61724eb649521586ae012d57202ec
Chaerul Umam <chaerulumamb244@gmail.com>
2023-01-26 23:41:47 +0700

    footer section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import './Footer.css';
+import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
+
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">Tangerang, Banten, 15710, Indonesia</p>
+        <p className="p__opensans">+62 813-1174-5927</p>
+        <p className="p__opensans">022-000988</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">
+          &quot;The best way to find yourself is to lose yourself in the service
+          of others.&quot;
+        </p>
+        <img
+          src={images.spoon}
+          alt="img_spoom"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiInstagram />
+          <FiTwitter />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer_copyright">
+      <p className="p__opensans">2023 JAMET. All Rights reserved</p>
+    </div>
   </div>
 );
 
